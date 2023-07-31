14fc452c9b45b33b1b4f1db344284a50f0331598
tahmid Rafi <tahmid-rafi@enlightsolution.com>
2023-07-31 09:47:36 +0600

    dev

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  properties: Array <any> = [
+    {
+      "Id": 1,
+      "Name": "Birla House",
+      "Type": "House",
+      "Price": 12000
+    },
+    {
+      "Id": 2,
+      "Name": "Birla House-2",
+      "Type": "House",
+      "Price": 120002
+    },
+    {
+      "Id": 3,
+      "Name": "Birla House-3",
+      "Type": "House",
+      "Price": 120003
+    },
+    {
+      "Id": 4,
+      "Name": "Birla House-4",
+      "Type": "House",
+      "Price": 120004
+    }
+]
+
   constructor() { }
 
   ngOnInit() {
